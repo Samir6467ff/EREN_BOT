7d37ded8df8c4aa8d76f519e0137e38af2889f48
1233taisiya <167587521+1233taisiya@users.noreply.github.com>
2024-06-19 12:16:59 +0300

    Update رونالدو.js

--- plugins/رونالدو.js
+++ plugins/رونالدو.js
@@ -2,46 +2,9 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/CristianoRonaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendFile(m.chat, ronaldo, 'error.jpg', `*siuuuuuuuuuu*`, m)}
+conn.sendButton(m.chat, `💞 ${mid.buscador}: ${text}`, wm, link, [['🔄 𝙎𝙞𝙜𝙪𝙞𝙚𝙣𝙩𝙚 | 𝙉𝙚𝙭𝙩', `/imagen ${text}`]], null, null, m)
+//conn.sendFile(m.chat, link, 'error.jpg', `💞 ${mid.buscador}: ${text}`, m)
 { generateWAMessageFromContent, proto } = pkg
-
-var handler = async (m, { conn, usedPrefix }) => {
-
-let msg = generateWAMessageFromContent(m.chat, {
-  viewOnceMessage: {
-    message: {
-        "messageContextInfo": {
-          "deviceListMetadata": {},
-          "deviceListMetadataVersion": 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: "test"
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "test"
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: "test",
-            subtitle: "test",
-            hasMediaAttachment: false
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-                              {
-                                  name: 'quick_reply',
-                                  buttonParamsJson: JSON.stringify({
-                                      display_text: 'التالي',
-                                      id: `.رونالدو`
-
-                                  })
-                              }
-           ],
-          })
-        })
-    }
-  }
-}, {})
 //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['⚽ NEXT ⚽', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['img']
