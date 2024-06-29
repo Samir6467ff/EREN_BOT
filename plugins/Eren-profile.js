305063475f6f5fabc85af3323c65febe5c664c0f
JstKira <jsalotaibi5@gmail.com>
2024-06-29 14:44:58 +0300

    Update Eren-profile.js

--- plugins/Eren-profile.js
+++ plugins/Eren-profile.js
@@ -1,41 +1,40 @@
+import { createHash } from 'crypto'
 import PhoneNumber from 'awesome-phonenumber'
-import fetch from 'node-fetch'
-let handler = async (m, { conn }) => {
-  let user = db.data.users[m.sender]
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-    let { premium, level, limit, exp, lastclaim, registered, regTime, age, pasangan } = global.db.data.users[m.sender]
-    let username = conn.getName(who)
-    let name = conn.getName(who)
-    let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
-    let str = `
-    *「✧|───✦❯┇💎┇❮✦───|✧」* 
-*⎔↞┃الاسم🀄 ↞* ${username} \n*⎔↞┃اسم🎖 ↞*Bio:* ' + about : ''}
-*⎔↞┃منشن☘️ ↞* @${who.replace(/@.+/, '')}
-*⎔↞┃لفلك📮 ↞* ${level}
-*⎔↞┃رابط⚡️ ↞* https://wa.me/${who.split`@`[0]}
-*⎔↞┃انذارات🔍↞* ${warn}/${maxwarn}
-*⎔↞┃شريك🍁 ↞*  ${pasangan ? `@${pasangan.split("@")[0]}` : `Tidak Punya`}
-*⎔↞┃مسجل📠 ↞* ${registered ? 'نعم': 'لا'}
-*⎔↞┃مميز🌷 ↞* ${premium ? "نعم" :"لا"}
-*⎔↞┃وقت مميز ↞⏱️* 
-${clockString(user.premiumTime)}
-*「✧|───✦❯┇💎┇❮✦───|✧」* 
-`.trim()
-   await conn.sendFile(m.chat, pp, '', str, m)
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, usedPrefix, command}) => {
+
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/killua.jpg')
+let user = global.db.data.users[who]
+let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+let username = conn.getName(who)
+let math = max - xp
+let prem = global.prems.includes(who.split`@`[0])
+let sn = createHash('md5').update(who).digest('hex')
+
+let str = `*❖ ── ✦ ──『⚜️』── ✦ ── ❖*
+*⤶❏ الاسم 👤:* ${username} ${registered ? '\n   • ' + name + ' ': ''}   
+*⤶❏ المنشن 📧 : @${who.replace(/@.+/, '')}*
+*⤶❏ الرقم ☎️ : ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}*
+*⤶❏ الرابط 🖇️ : wa.me/${who.split`@`[0]}${registered ? '\n⤶❏ *🎈العمر*: ' + age + ' years' : ''}*
+*⤶❏ التحذيرات ⛔ : ${warn}/${maxwarn}*
+*⤶❏ الجواهر 💎 : ${diamond}*
+*⤶❏ المستوى 📊 : ${level}*
+*⤶❏ الاكس بي 📈* : المجموع ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `*${usedPrefix}levelup*` : `باقي لك  *${math}اكس بي للصعود الى لفل اخر*`}
+*⤶❏ التصنيف 🧮 : ${role}*
+*⤶❏ التسجيل 📄 : ${registered ? 'يب': 'لا'}*
+*⤶❏ بريميوم 🌩️ : ${prem ? 'يب' : 'لا'}*
+*❖ ── ✦ ──『⚜️』── ✦ ── ❖*`
+    conn.sendFile(m.chat, pp, 'perfil.jpg', str, m, false, { mentions: [who] })
+
+
 }
-handler.help = ['profile']
-handler.tags = ['main']
-handler.command = /^البروفايل$/i
+handler.help = ['perfil']
+handler.tags = ['group']
+handler.command = ['profile', 'بروفايليي'] 
+
 export default handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, '*Hari*\n ', h, ' *Jam*\n ', m, ' *Menit*\n ', s, ' *Detik* '].map(v => v.toString().padStart(2, 0)).join('')
-}
