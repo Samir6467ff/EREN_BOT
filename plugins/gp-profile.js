734f52ec1904d3b9679d2490b1df37bccc6b37de
JstKira <jsalotaibi5@gmail.com>
2024-06-30 18:38:38 +0300

    Update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -1,6 +1,6 @@
-import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { createHash } from 'crypto';
+import PhoneNumber from 'awesome-phonenumber';
+import { canLevelUp, xpRange } from '../lib/levelling.js';
 //import db from '../lib/database.js'
 
 let handler = async (m, { conn, usedPrefix, command}) => {
