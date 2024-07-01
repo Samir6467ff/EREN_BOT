85f0e24a5ca7830f3d54dc9a44a41199db26d1f0
JstKira <jsalotaibi5@gmail.com>
2024-07-01 23:51:09 +0300

    Update تخفيض demote.js

--- plugins/تخفيض demote.js
+++ plugins/تخفيض demote.js
@@ -1,33 +1,34 @@
-let handler = async (m, { conn,usedPrefix, text }) => {
-if(isNaN(text) && !text.match(/@/g)){
-	
-}else if(isNaN(text)) {
-var number = text.split`@`[1]
-}else if(!isNaN(text)) {
-var number = text
-}
-	
-if(!text && !m.quoted) return conn.reply(m.chat, `*[❗] الاستخدام المناسب*\n\n*┯┷*\n*┠≽ ${usedPrefix}تخفيض @منشن*\n*┠≽ ${usedPrefix}تخفيض -> الرد على رسالة*\n*┷┯*`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] الرقم الذي تم إدخاله غير صحيح ، الرجاء إدخال الرقم الصحيح*`, m)
-  
-try {
-if(text) {
-var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-var user = m.quoted.sender
-} else if(m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, `*┃🚫┃┊✓ تم تنزيله من الاشراف تبأ لك كنت ادمن فاشل 🥲↞*`, m)
-}}
-handler.help = ['*967773685143xxx*','*@اسم المستخدم*','*محادثة المستجيب*'].map(v => 'demote ' + v)
-handler.tags = ['group']
-handler.command = /^(خفض|إزالة المشرف|ازالة المشرف|تاخفيض|تنزيل)$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
-export default handler
+const handler = async (m, {conn, usedPrefix, text}) => {
+  if (isNaN(text) && !text.match(/@/g)) {
+
+  } else if (isNaN(text)) {
+    var number = text.split`@`[1];
+  } else if (!isNaN(text)) {
+    var number = text;
+  }
+
+  if (!text && !m.quoted) return conn.reply(m.chat, `*[❗] استخدام صحيح*\n\n*┯┷*\n*┠≽ ${usedPrefix}الامر الموضح @منشن*\n*┠≽ ${usedPrefix}تخفيض الادمن -> قم بالرد على الرسالة*\n*┷┯*`, m);
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] الرقم الذي أدخلته غير صحيح، يرجى إدخال رقم صحيح*`, m);
+
+  try {
+    if (text) {
+      var user = number + '@s.whatsapp.net';
+    } else if (m.quoted.sender) {
+      var user = m.quoted.sender;
+    } else if (m.mentionedJid) {
+      var user = number + '@s.whatsapp.net';
+    }
+  } catch (e) {
+  } finally {
+    conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+    conn.reply(m.chat, `*[ ✅ ] تم تنزيله من الاشراف ،نشكر جهوده ونتمنى له التوفيق في المستقبل*`, m);
+  }
+};
+handler.help = ['*2011xxx*', '*@usuario*', '*responder chat*'].map((v) => 'demote ' + v);
+handler.tags = ['group'];
+handler.command = /^(demote|إزالة المشرف|ازالة المشرف|خفض|تخفيض)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+export default handler;
