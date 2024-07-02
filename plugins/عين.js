b106615eda8ac0028d2d5f50efb13a7e9664c853
JstKira <jsalotaibi5@gmail.com>
2024-07-03 00:46:33 +0300

    Update عين.js

--- plugins/عين.js
+++ plugins/عين.js
@@ -1,4 +1,3 @@
-
 let timeout = 60000
 let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
@@ -8,28 +7,25 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://raw.githubusercontent.com/1233taisiya/EREN_BOT/master/plugins/%D9%81%D8%B1%D9%8A%D8%A8%D9%8A%D9%83.js')).json()
+    let src = await (await fetch('https://gist.githubusercontent.com/Kyutaka101/4e01c190b7d67225ad7a86d388eeedf6/raw/67f0de059cea4b965a3f3bf211c12fc9c48043e5/gistfile1.txt')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*˼‏❖˹┇⇠『السؤال ${command.toUpperCase()}』*
-  *「✧|────✦❯◇❮✦────|✧」*
-  *〄↞┇الـوقـت⏱️↞ ${(timeout / 1000).toFixed(2)}┇*
-  *〄↞┇الـجـائـزة🎖↞ ${poin} ┇*
-  *「✧|────✦❯◇❮✦────|✧」*
-   > استخدم انسحب للأنسحاب‼️
-   *✧━ • ━ 「 ✤ 」 ━ • ━✧*
-   > ❯⏐ 𝐵𝛩𝑇 𝑀𝐼𝑍𝑈𝐻𝐴𝑅𝐴
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+『بوت العتبان』
      `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `*❮ 💧┇انتهي الوقت┇💧❯*\n*⎔↞┇الاجـابـة⚡↞ ${json.name}┇*`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
 }
-handler.help = ['guessflag']
-handler.tags = ['game']
+handler.help = ['عين']
+handler.tags = ['fun']
 handler.command = /^عين/i
 
 export default handler
