4530a122c103d2738004709718bd9b638f777c8c
JstKira <jsalotaibi5@gmail.com>
2024-07-03 23:35:52 +0300

    Update القائمة.js

--- plugins/القائمة.js
+++ plugins/القائمة.js
@@ -1,103 +1,99 @@
-let handler = async (m, { conn, args,
-usedPrefix, command }) => {
-conn.relayMessage(m.chat, {
-viewOnceMessage: {
-message: {
-interactiveMessage: {
-header: {
-title: '> ⧉┆مرحبا ياعزيزي 😍 ⤌⤈\n> ↝👋🏻↜\n> ⧉┆انا بوت واتساب'
- },
- body: {
- text: '> ♩☜ اعمل في الخاص وجروبات\n> ♩☜ وظيفتي حماية جروبك\n> ♩☜اوامر كلمه .الاوامر لمعرفه\> n♩☜ اوامر البوت وطريقه الاستخدام\n> ♩☜ مميزات البوت كثيره جدا\n> ♩☜ ويعمل بجودة فائقه وعاليه\n> 𓍹————————————𓍻\n> ↜★ اوامر سورس بوت العتبان ★↝\n> 𓍹————————————𓍻'
-  },
-  nativeFlowMessage: {
-  buttons: [
-   {
-  name: 'single_select',
-  buttonParamsJson: JSON.stringify({
-  title: '❰ اختار من القائمه📄 ❱',
-  sections: [
-  {
-  title: 'قسم الاوامر',
-  highlight_label: 'بوت بلاك & عفروتو',
-  rows: [
-  {
-  header: 'يعطيك اوامر القروبات',
-  title: '.ميزو1',
-  description: '',
-  id: '.ميزو1'
-  },
-  {
-  header: 'يعطيك اوامر الاعضاء',
-  title: '.ميزو2',
-  description: '',
-  id: '.ميزو2'
-  },
-  {
-   header: 'يعطيك اوامر الترفيه',
-  title: '.ميزو3',
-  description: '',
-  id: '.ميزو3'
-  },
-   {
-    header: 'يعطيك اوامر التحميل',
-  title: 'التحميل',
-  description: '',
-  id: '.ميزو4'
-  },
-   {
-    header: 'يعطيك اوامر الصور',
-  title: '.ميزو5',
-  description: '',
-  id: '.ميزو5'
-  },
-   {
-    header: 'يعطيك اوامر اللفل',
-  title: '.ميزو6',
-  description: '',
-  id: '.ميزو6'
-  },
-   {
-   header: 'يعطيك اوامر الملصقات',
-  title: 'الملصقات',
-  description: '',
-  id: '.ميزو7'
-  },
-  {
-    header: 'يعطيك اوامر البحث و التنزيل',
-  title: 'البحث و التنزيل',
-  description: '',
-  id: '.ميزو8'
-  },
-  {
-  header: 'يعطيك اوامر التحويل',
-  title: 'التحويل',
-  description: '',
-  id: '.ميزو9'
-  },
-  {
-  header: 'يعطيك كل الاامر',
-  title: '.ميزو12',
-  description: '',
-  id: '.ميزو12'
-  }
-  ]
-  }
-  ]
-  }),
-  messageParamsJson: ''
-  }
-  ]
-  }
-  }
-  }
-  }
-  }, {})
+import { createHash } from 'crypto'
+   import PhoneNumber from 'awesome-phonenumber'
+   import { canLevelUp, xpRange } from '../lib/levelling.js'
+   import fetch from 'node-fetch'
+   import fs from 'fs'
+   const { levelling } = '../lib/levelling.js'
+   import moment from 'moment-timezone'
+   import { promises } from 'fs'
+   import { join } from 'path'
+   const time = moment.tz('Egypt').format('HH')
+   let wib = moment.tz('Egypt').format('HH:mm:ss')
+   //import db from '../lib/database.js'
 
-  }
+   let handler = async (m, { conn, usedPrefix, command}) => {
+       let d = new Date(new Date + 3600000)
+       let locale = 'ar'
+       let week = d.toLocaleDateString(locale, { weekday: 'long' })
+       let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+       let _uptime = process.uptime() * 1000
+       let uptime = clockString(_uptime)
+   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+   if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات الخاصة بي`
+   let videoUrl = 'https://telegra.ph/file/6f5728ffb36e986b957f6.mp4'
+   let user = global.db.data.users[who]
+   let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
+   let { min, xp, max } = xpRange(user.level, global.multiplier)
+   let username = conn.getName(who)
+   let math = max - xp
+   let prem = global.prems.includes(who.split`@`[0])
+   let sn = createHash('md5').update(who).digest('hex')
+   let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+   let more = String.fromCharCode(8206)
+   let readMore = more.repeat(850) 
+  m.react('📁')
+   let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+   let str = `
+      ┓━ ╼━━━╃⌬〔 𝙼𝚅𝚁𝙾 𝙱𝙾𝚃 〕⌬╄━━━╾ ━┏
+ مـرحــبـا ${taguser} 👋🏻
 
-  handler.help = ['info']
-  handler.tags = ['main']
-  handler.command = ['مهام','اوامر','er','youssef','ui','op']
+≼👤≽ مـعـلــومـات الـبــوت╿↶
+━ ── • ꕤ • ── ━
+ اســم الـبــوت  𝙼𝚅𝚁𝙾 お ‘ 
+منـصـه التـشغيــل 【.هيروكو.】
+وقت التشغيل : ${uptime}
+اليوم : ${week}
+التاريخ : ${date}
+مطوري : wa.me/201208386317
+⋄━───═◞⬪⇊⬪◟═───━⋄
+اذا لم يرد عليك البوت عليك ارسال 
+⚡⇇.صلح 
+⚡⇇.تصليح
+━ ── • ꕤ • ── ━
+⇊ قوائم بوت مارو المطلوبه ⇊
+.....................
+🤖⇇.ذكاءاصطناعي
+⛩️⇇.الجروب
+🔮⇇.الانمي
+👥⇇.الاعضاء
+🕋⇇.الدين
+💎⇇.البنك
+📥⇇.التحميلات
+🎞⇇.الايديت
+⚙️⇇.الادوات
+♻️⇇.التحويلات
+🎮⇇.الترفيه
+🔊⇇.الاصوات
+👑⇇.المطور
+💢⇇.تصاميم
+📌⇇. قائمتي
+⚡⇇.المعرف 
+💯⇇.السورس
+📮⇇.قوانين
+━ ── • ꕤ • ── ━
+👋🏻 هلا اذا اردت الوصول اللي الاوامر مره واحده كامله ارسل
 
-  export default handler
+ 🚩⇇.المهام
+
+⋄━───═◞⬪قوانين⬪◟═───━⋄
+❏╎❯ ممنوع سب البوت لانك سبيت البوت = سبيت المطور
+❏╎❯ تمتع بالبوت ولا تكتر اسبام للبوت اذا كان لديك مشكله او تريد اضافه اوامر اخري جديده تواصل مع المطور
+❏╎❯ المطور wa.me/201208386317
+*┛━ ╼━━━╃⌬〔 𝙼𝚅𝚁𝙾 𝙱𝙾𝚃 〕⌬╄━━━╾ ━┗*
+   `.trim()
+       conn.sendMessage(m.chat, {
+           video: { url: videoUrl }, caption: str,
+     mentions: [m.sender,global.conn.user.jid],
+     gifPlayback: true,gifAttribution: 0
+       }, { quoted: m });
+   };
+   handler.help = ['main']
+   handler.command = /^(الاوامر|menu|أوامر|اوامر)$/i
+
+   export default handler
+   function clockString(ms) {
+       let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+       let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+       let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+       return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+      
