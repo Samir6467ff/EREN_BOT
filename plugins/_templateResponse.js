faaf54499f39bb12b90f3a9ab0b0dbc14be5d39e
JstKira <jsalotaibi5@gmail.com>
2024-07-03 19:02:34 +0300

    Update _templateResponse.js

--- plugins/_templateResponse.js
+++ plugins/_templateResponse.js
@@ -1,31 +1,82 @@
-import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysockets/baileys';
+// @type {import('@whiskeysockets/baileys')}
 
-const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
-    const mentionId = m.key.participant || m.key.remoteJid;
-
-    try {
-        var joanimiimg = await prepareWAMessageMedia({ image: { url: 'https://telegra.ph/file/48d1b1b70fcc002571a78.jpg' } }, { upload: conn.waUploadToServer });
-        const buttons = [
-            { buttonId: 'id1', buttonText: { displayText: 'Button 1' }, type: 1 },
-            { buttonId: 'id2', buttonText: { displayText: 'Button 2' }, type: 1 },
-            { buttonId: 'id3', buttonText: { displayText: 'Button 3' }, type: 1 }
-        ];
-
-        const buttonMessage = {
-            contentText: `Your custom message here`.trim(),
-            footerText: `Footer text here`.trim(),
-            buttons: buttons,
-            headerType: 4,
-            imageMessage: joanimiimg.imageMessage
-        };
-
-        await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
-    } catch (error) {
-        console.error('Error in preparing media:', error);
-    }
-};
-
-handler.help = ['template'];
-handler.tags = ['For Test'];
-handler.command = /^(template|templatecmd)$/i;
-export default handler;
+const { proto, generateWAMessage, areJidsSameUser, decryptPollVote, } = (await import('@whiskeysockets/baileys')).default;
+export async function all(m, chatUpdate) {
+if (m.isBaileys) {
+return
+}
+if (!m.message) {
+return
+}
+if (!(m.message.buttonsResponseMessage || m.message.templateButtonReplyMessage || m.message.listResponseMessage || m.message.interactiveResponseMessage)) {
+return
+}
+let id
+if (m.message.buttonsResponseMessage) {
+id = m.message.buttonsResponseMessage.selectedButtonId
+} else if (m.message.templateButtonReplyMessage) {
+id = m.message.templateButtonReplyMessage.selectedId
+} else if (m.message.listResponseMessage) {
+id = m.message.listResponseMessage.singleSelectReply?.selectedRowId;
+} else if (m.message.interactiveResponseMessage) {
+id = JSON.parse(m.message.interactiveResponseMessage.nativeFlowResponseMessage.paramsJson).id
+}
+const text = m.message.buttonsResponseMessage?.selectedDisplayText || m.message.templateButtonReplyMessage?.selectedDisplayText || m.message.listResponseMessage?.title
+let isIdMessage = false
+let usedPrefix
+for (const name in global.plugins) {
+const plugin = global.plugins[name]
+if (!plugin) {
+continue
+}
+if (plugin.disabled) {
+continue
+}
+if (!opts['restrict']) {
+if (plugin.tags && plugin.tags.includes('admin')) {
+continue
+}}
+if (typeof plugin !== 'function') {
+continue
+}
+if (!plugin.command) {
+continue
+}
+const str2Regex = (str) => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+const _prefix = plugin.customPrefix ? plugin.customPrefix : this.prefix ? this.prefix : global.prefix
+const match = (_prefix instanceof RegExp ? [[_prefix.exec(id), _prefix]] : Array.isArray(_prefix) ? _prefix.map((p) => {
+const re = p instanceof RegExp ? p : new RegExp(str2Regex(p));
+return [re.exec(id), re]
+}) :
+typeof _prefix === 'string' ?
+[[new RegExp(str2Regex(_prefix)).exec(id), new RegExp(str2Regex(_prefix))]] :
+[[[], new RegExp]]
+).find((p) => p[1])
+if ((usedPrefix = (match[0] || '')[0])) {
+const noPrefix = id.replace(usedPrefix, '')
+let [command] = noPrefix.trim().split` `.filter((v) => v)
+command = (command || '').toLowerCase()
+const isId = plugin.command instanceof RegExp ?
+plugin.command.test(command) :
+Array.isArray(plugin.command) ?
+plugin.command.some((cmd) => cmd instanceof RegExp ?
+cmd.test(command) :
+cmd === command,
+) :
+typeof plugin.command === 'string' ?
+plugin.command === command :
+false
+if (!isId) {
+continue
+}
+isIdMessage = true
+}}
+const messages = await generateWAMessage(m.chat, {text: isIdMessage ? id : text, mentions: m.mentionedJid}, {
+userJid: this.user.id,
+quoted: m.quoted && m.quoted.fakeObj,
+})
+messages.key.fromMe = areJidsSameUser(m.sender, this.user.id)
+messages.key.id = m.key.id
+messages.pushName = m.name
+if (m.isGroup) {
+messages.key.participant = messages.participant = m.sender
